39b20bb2c8f35f0b09b50ccd5348faf924e3093f
kzzt <321jkl633pQr80>
2021-07-19 08:04:45 -0500

    successfully ran - 7/19

--- working_scripts/daily_conduit_report.py
+++ working_scripts/daily_conduit_report.py
@@ -1,39 +1,17 @@
 # queries VMI po from the previous week and outputs to file named for PONUMs
-import cx_Oracle
 from xlrd import open_workbook
-
+import connect as ct
 import time
 import pandas as pd
-from sqlalchemy import create_engine
-
 import win32com.client as win32
 
-import onccfg as cfg
-
-user = cfg.username
-password = cfg.password
-sid = cx_Oracle.makedsn(cfg.host, cfg.port, sid=cfg.sid)  # host, port, sid
-
-cstr = f"oracle://{user}:{password}@{sid}"
-
-engine = create_engine(
-    cstr,
-    convert_unicode=False,
-    pool_recycle=20,
-    pool_size=100,
-    echo=False,
-    max_identifier_length=128,
-)
-
-sql_file = open(cfg.conduit_query, "r")
-conduit_query = sql_file.read()
-sql_file.close()
+conduit_query = ct.query(ct.conduit_query)
 
 timestring = time.strftime("%m.%d.%y")
 
 
 def main():
-    conduit_df = pd.DataFrame(pd.read_sql_query(conduit_query, engine))
+    conduit_df = pd.DataFrame(pd.read_sql_query(conduit_query, ct.connect()))
     conduit_df.columns = map(str.upper, conduit_df.columns)
 
     writer = pd.ExcelWriter(
@@ -58,12 +36,14 @@ if __name__ == "__main__":
     mail.To = "David.Kole@oncor.com; Gianmarco.Russo@oncor.com; Christy.Leonard@oncor.com; Kevin.Vaughan@oncor.com"
     # mail.To = "Kevin.Vaughan@oncor.com"
     mail.Subject = f"Daily 886 Conduit Inventory Report {timestring}"
-    mail.Attachments.Add(f"c:\\users\\u6zn\\desktop\\xlsx\\Daily 886 Conduit Inventory Report {timestring}.xlsx")
+    mail.Attachments.Add(
+        f"c:\\users\\u6zn\\desktop\\xlsx\\Daily 886 Conduit Inventory Report {timestring}.xlsx"
+    )
     mail.HtmlBody = (
-        "<html xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:w=\"urn:schemas-microsoft-com:office:word\" xmlns:m=\"http://schemas.microsoft.com/office/2004/12/omml\" xmlns=\"http://www.w3.org/TR/REC-html40\"><head><meta http-equiv=Content-Type content=\"text/html; charset=us-ascii\"><meta name=Generator content=\"Microsoft Word 15 (filtered medium)\"><style><!--\n"
+        '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head><meta http-equiv=Content-Type content="text/html; charset=us-ascii"><meta name=Generator content="Microsoft Word 15 (filtered medium)"><style><!--\n'
         "/* Font Definitions */\n"
         "@font-face\n"
-        "	{font-family:\"Cambria Math\";\n"
+        '	{font-family:"Cambria Math";\n'
         "	panose-1:2 4 5 3 5 4 6 3 2 4;}\n"
         "@font-face\n"
         "	{font-family:Calibri;\n"
@@ -76,7 +56,7 @@ if __name__ == "__main__":
         "	{margin:0in;\n"
         "	margin-bottom:.0001pt;\n"
         "	font-size:11.0pt;\n"
-        "	font-family:\"Calibri\",sans-serif;}\n"
+        '	font-family:"Calibri",sans-serif;}\n'
         "a:link, span.MsoHyperlink\n"
         "	{mso-style-priority:99;\n"
         "	color:#0563C1;\n"
@@ -87,15 +67,15 @@ if __name__ == "__main__":
         "	text-decoration:underline;}\n"
         "span.EmailStyle17\n"
         "	{mso-style-type:personal;\n"
-        "	font-family:\"Calibri\",sans-serif;\n"
+        '	font-family:"Calibri",sans-serif;\n'
         "	color:windowtext;}\n"
         "span.EmailStyle18\n"
         "	{mso-style-type:personal;\n"
-        "	font-family:\"Calibri\",sans-serif;\n"
+        '	font-family:"Calibri",sans-serif;\n'
         "	color:#1F497D;}\n"
         "span.EmailStyle19\n"
         "	{mso-style-type:personal-reply;\n"
-        "	font-family:\"Calibri\",sans-serif;\n"
+        '	font-family:"Calibri",sans-serif;\n'
         "	color:#1F497D;}\n"
         ".MsoChpDefault\n"
         "	{mso-style-type:export-only;\n"
@@ -106,14 +86,18 @@ if __name__ == "__main__":
         "div.WordSection1\n"
         "	{page:WordSection1;}\n"
         "--></style><!--[if gte mso 9]><xml>\n"
-        "<o:shapedefaults v:ext=\"edit\" spidmax=\"1026\" />\n"
+        '<o:shapedefaults v:ext="edit" spidmax="1026" />\n'
         "</xml><![endif]--><!--[if gte mso 9]><xml>\n"
-        "<o:shapelayout v:ext=\"edit\">\n"
-        "<o:idmap v:ext=\"edit\" data=\"1\" />\n"
-        f"</o:shapelayout></xml><![endif]--></head><body lang=EN-US link=\"#0563C1\" vlink=\"#954F72\"><div class=WordSection1><p class=MsoNormal><span style='color:#1F497D'>Good morning,</span><span style='color:#1F497D'><o:p></o:p></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='color:#1F497D'>Attached is the daily 886 conduit inventory report for {timestring}.<o:p></o:p></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='color:#1F497D'>Thanks,</span><o:p></o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><b><span style='font-family:\"Verdana\",sans-serif;color:#7F7F7F'>Kevin Vaughan<o:p></o:p></span></b></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:gray'>Material Management Analyst<o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><b><span style='font-family:\"Verdana\",sans-serif;color:#717073'>Oncor | Supply Chain Management<o:p></o:p></span></b></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:#717073;mso-fareast-language:ZH-CN'>15101 Trinity Blvd. <o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:#717073;mso-fareast-language:ZH-CN'>Fort Worth, TX 76155<o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:gray'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif;color:gray'>Tel: 817.359.2310<o:p></o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif;color:#7F7F7F'>Cell: 214.542.6626<o:p></o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif'><a href=\"mailto:Kevin.Vaughan@oncor.com\">Kevin.Vaughan@oncor.com</a><span style='color:#2E74B5'><o:p></o:p></span></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><b><span style='font-family:\"Arial\",sans-serif;color:#2E74B5'>oncor.com<o:p></o:p></span></b></p><p class=MsoNormal><o:p>&nbsp;</o:p></p></div></body></html>")
+        '<o:shapelayout v:ext="edit">\n'
+        '<o:idmap v:ext="edit" data="1" />\n'
+        f"</o:shapelayout></xml><![endif]--></head><body lang=EN-US link=\"#0563C1\" vlink=\"#954F72\"><div class=WordSection1><p class=MsoNormal><span style='color:#1F497D'>Good morning,</span><span style='color:#1F497D'><o:p></o:p></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='color:#1F497D'>Attached is the daily 886 conduit inventory report for {timestring}.<o:p></o:p></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='color:#1F497D'>Thanks,</span><o:p></o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><b><span style='font-family:\"Verdana\",sans-serif;color:#7F7F7F'>Kevin Vaughan<o:p></o:p></span></b></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:gray'>Material Management Analyst<o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><b><span style='font-family:\"Verdana\",sans-serif;color:#717073'>Oncor | Supply Chain Management<o:p></o:p></span></b></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:#717073;mso-fareast-language:ZH-CN'>15101 Trinity Blvd. <o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:#717073;mso-fareast-language:ZH-CN'>Fort Worth, TX 76155<o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><span style='font-family:\"Verdana\",sans-serif;color:gray'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif;color:gray'>Tel: 817.359.2310<o:p></o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif;color:#7F7F7F'>Cell: 214.542.6626<o:p></o:p></span></p><p class=MsoNormal><span style='font-family:\"Verdana\",sans-serif'><a href=\"mailto:Kevin.Vaughan@oncor.com\">Kevin.Vaughan@oncor.com</a><span style='color:#2E74B5'><o:p></o:p></span></span></p><p class=MsoNormal><span style='color:#1F497D'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><b><span style='font-family:\"Arial\",sans-serif;color:#2E74B5'>oncor.com<o:p></o:p></span></b></p><p class=MsoNormal><o:p>&nbsp;</o:p></p></div></body></html>"
+    )
 
     # mail.Send()  ## uncomment or it won't work
-    xl_workbook = open_workbook(f"c:\\users\\u6zn\\desktop\\xlsx\\Daily 886 Conduit Inventory Report {timestring}.xlsx", on_demand=True)
+    xl_workbook = open_workbook(
+        f"c:\\users\\u6zn\\desktop\\xlsx\\Daily 886 Conduit Inventory Report {timestring}.xlsx",
+        on_demand=True,
+    )
     xl_sheet = xl_workbook.sheet_by_index(0)
 
     mail.Display(False)
--- working_scripts/irby_audit_report.py
+++ working_scripts/irby_audit_report.py
@@ -1,64 +1,50 @@
 # queries VMI po from the previous week and outputs to file named for PONUMs
-import cx_Oracle
 from xlrd import open_workbook
+
+import connect as ct
 
 import time
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
 
 import win32com.client as win32
 
-import onccfg as cfg
+filename = "c:\\users\\u6zn\\downloads\\WO_Shipment_Update_20210717_230002.dat"
 
-work_orders = (
-    "'15644378', '15741485', '15783326', '15928601', '15928714', '15962970', '15962990', '15971429', "
-    "'15971507', '15971508', '15996618', '15996622', '16004535', '16018053' "
-)
+df = pd.read_csv(filename, skiprows=1)
+df = df.loc[df['SHIP_STATUS'] == 'LANE']
 
-cost_query = (
-    " SELECT\n"
-    "	WO.PARENT,\n"
-    "	WP.WONUM, WP.LOCATION ,\n"
-    "	WP.ITEMNUM,\n"
-    "	WP.ITEMQTY ,\n"
-    "	WP.LINECOST ,\n"
-    "	WP.UNITCOST, U1.MEMO\n"
-    "FROM\n"
-    "	MXRADS.WPMATERIAL WP\n"
-    "LEFT JOIN (\n"
-    "	SELECT\n"
-    "		WONUM,\n"
-    "		PARENT,\n"
-    "		ESTMATCOST\n"
-    "	FROM\n"
-    "		MXRADS.WORKORDER\n"
-    "	WHERE\n"
-    f"		WONUM IN ({work_orders})) WO ON\n"
-    "	WO.WONUM = WP.WONUM\n"
-    "	LEFT JOIN (SELECT ITEMNUM, STORELOC, QUANTITY, PONUM, REFWO, MEMO FROM MSCRADS.MATUSETRANS) U1 ON U1.ITEMNUM = WP.ITEMNUM AND U1.STORELOC = WP.LOCATION AND U1.REFWO = WP.WONUM\n"
-    "WHERE\n"
-    f"	WP.WONUM IN ({work_orders})\n"
-    "ORDER BY\n"
-    "	WO.PARENT,\n"
-    "	WP.WONUM,\n"
-    "	WP.ITEMNUM "
-)
 
-user = cfg.username
-password = cfg.password
-sid = cx_Oracle.makedsn(cfg.host, cfg.port, sid=cfg.sid)  # host, port, sid
+work_orders = ct.irby_workorders()
 
-cstr = f"oracle://{user}:{password}@{sid}"
-
-engine = create_engine(
-    cstr,
-    convert_unicode=False,
-    pool_recycle=20,
-    pool_size=100,
-    echo=False,
-    max_identifier_length=128,
-)
+cost_query = ("SELECT\n"
+              "	WO.PARENT,\n"
+              "	WP.WONUM, WP.LOCATION , SHIPTO.SHIPTO, SHIPTO.SHIPTONAME,\n"
+              "	WP.ITEMNUM,\n"
+              "	WP.ITEMQTY ,\n"
+              "	WP.LINECOST ,\n"
+              "	WP.UNITCOST, U1.MEMO\n"
+              "FROM\n"
+              "	MXRADS.WPMATERIAL WP\n"
+              "LEFT JOIN (\n"
+              "	SELECT\n"
+              "		WONUM,\n"
+              "		PARENT,\n"
+              "		ESTMATCOST\n"
+              "	FROM\n"
+              "		MXRADS.WORKORDER\n"
+              "	WHERE\n"
+              f"		WONUM IN ({work_orders})) WO ON\n"
+              "	WO.WONUM = WP.WONUM\n"
+              "	LEFT JOIN (SELECT ITEMNUM, STORELOC, QUANTITY, PONUM, REFWO, LOCATION, MEMO FROM MSCRADS.MATUSETRANS) U1 ON U1.ITEMNUM = WP.ITEMNUM AND U1.STORELOC = WP.LOCATION AND U1.REFWO = WP.WONUM\n"
+              f"LEFT JOIN (SELECT WONUM, DESCRIPTION AS SHIPTONAME, SADDRESSCODE AS SHIPTO FROM MXRADS.WOSERVICEADDRESS WHERE WONUM IN ({work_orders})) SHIPTO ON SHIPTO.WONUM = WP.WONUM\n"
+              "	\n"
+              "	WHERE\n"
+              f"	WP.WONUM IN ({work_orders})\n"
+              "ORDER BY\n"
+              "	WO.PARENT,\n"
+              "	WP.WONUM,\n"
+              "	WP.ITEMNUM ")
 
 # cost_data_sql = "C:\\Users\\u6zn\\AppData\\Roaming\\DBeaverData\\workspace6\\General\\Scripts\\IRBY_COST.sql"
 
@@ -67,7 +53,7 @@ timestring = time.strftime("%m.%d.%y")
 
 
 def main():
-    cost_df = pd.DataFrame(pd.read_sql_query(cost_query, engine))
+    cost_df = pd.DataFrame(pd.read_sql_query(cost_query, ct.connect()))
     cost_df.columns = map(str.upper, cost_df.columns)
     print(cost_df.dtypes)
     cost_df["WONUM"] = cost_df["WONUM"].astype(int)
@@ -80,7 +66,7 @@ def main():
     cost_df.to_excel(irby_workbook, sheet_name="Irby WOs", index=False)
 
     cost_df_pivot = (
-        cost_df.groupby(["PARENT", "WONUM", "LOCATION"])
+        cost_df.groupby(["PARENT", "WONUM", "LOCATION", "SHIPTO"])
         .agg(ESTMATCOST=pd.NamedAgg(column="LINECOST", aggfunc=np.sum),)
         .reset_index()
     )
